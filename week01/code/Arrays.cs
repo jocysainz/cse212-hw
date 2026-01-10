159a0f89ac233908949feafa782ee0081cc3247a
jocysainz <lauragrajeda29@yahoo.com>
2026-01-10 14:15:46 -0600

    W01 Code: Dynamic Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,18 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // plan
+        // I will create a new double array of the lenght that was specified
+        // Then create a loop from 0 to lenght -1
+        // for each position in the array I will set the value to number * (i + 1)
+        // Then lastly I will return the array
+
+        double[] result = new double[length];
+        for (int i = 0; i < length; i++)
+        {
+            result[i] = number * (i + 1);
+        }
+        return result;
     }
 
     /// <summary>
@@ -29,5 +40,17 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // plan
+        // I will determine the index where the rotation will probably start which is data.Count - amount
+        // Then get the last amount of numbers and store them in a temporary list
+        // After that, remove the last amount elements from the og list while using RemoveRange(startIndex, amount)
+        // Finally, insert the temporary list at the start of the og list using InsertRange(0, tempList)
+
+        int startIndex = data.Count - amount;
+        List<int> temp = data.GetRange(startIndex, amount);
+        data.RemoveRange(startIndex, amount);
+        data.InsertRange(0, temp);
+
     }
 }
