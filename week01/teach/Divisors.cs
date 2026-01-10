e1f640cd317cce4889edbee41140eeb9e79e7d7f
jocysainz <lauragrajeda29@yahoo.com>
2026-01-10 13:40:47 -0600

    Week 1

--- week01/teach/Divisors.cs
+++ week01/teach/Divisors.cs
@@ -1,12 +1,20 @@
-public static class Divisors {
+using System;
+using System.Collections.Generic;
+
+public static class Divisors
+{
     /// <summary>
     /// Entry point for the Divisors class
     /// </summary>
-    public static void Run() {
+    public static void Run()
+    {
         List<int> list = FindDivisors(80);
-        Console.WriteLine("<List>{" + string.Join(", ", list) + "}"); // <List>{1, 2, 4, 5, 8, 10, 16, 20, 40}
+        Console.WriteLine("<List>{" + string.Join(", ", list) + "}");
+        // Expected: <List>{1, 2, 4, 5, 8, 10, 16, 20, 40}
+
         List<int> list1 = FindDivisors(79);
-        Console.WriteLine("<List>{" + string.Join(", ", list1) + "}"); // <List>{1}
+        Console.WriteLine("<List>{" + string.Join(", ", list1) + "}");
+        // Expected: <List>{1}
     }
 
     /// <summary>
@@ -16,9 +24,18 @@ public static class Divisors {
     /// </summary>
     /// <param name="number">The number to find the divisor</param>
     /// <returns>List of divisors</returns>
-    private static List<int> FindDivisors(int number) {
+    private static List<int> FindDivisors(int number)
+    {
         List<int> results = new();
-        // TODO problem 1
+
+        for (int i = 1; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                results.Add(i);
+            }
+        }
+
         return results;
     }
 }
